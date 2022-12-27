6a5679e7482eee5c9e98aa1d10be57a7f8d45805
Ivan Georgiev Programming <IvanGeorgievProgramming@gmail.com>
2022-12-27 20:07:56 +0200

    Free

--- HelloWorld/HelloWorld.c
+++ HelloWorld/HelloWorld.c
@@ -3,10 +3,12 @@
 
 int main(){
     char *name = malloc(20 * sizeof(char));
+
     printf("Enter your name: ");
     scanf("%s", name);
-
     printf("Hello %s", name);
+
+    free(name);
 
     return 0;
 }
