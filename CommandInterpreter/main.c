c266f8195028652b2a5d7bc3a712de690282db63
Ivan Georgiev Programming <IvanGeorgievProgramming@gmail.com>
2023-01-21 23:59:28 +0200

    Adding comments: main.c

--- CommandInterpreter/main.c
+++ CommandInterpreter/main.c
@@ -1,3 +1,5 @@
+// * Importing libraries
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +11,8 @@
 #include <fcntl.h>
 #include <time.h>
 
+// * Function declarations
+
 int numRows(int file, int* errorDetected);
 int* sizeRows(int file, int* errorDetected);
 char** getRows(int file, int* errorDetected);
@@ -18,15 +22,24 @@ char** getWords(char* row, int* errorDetected);
 void errorHandler(int errorDetected);
 void executeCommands(char* filename);
 
+// * Main function
+
 int main(int argc, char *argv[]){
+    // Check if the number of arguments is correct
     if(argc != 2){
+        // If not, print an error message and exit
         errx(1, "Invalid number of arguments");
     }
     
+    // If the number of arguments is correct, execute the commands
     executeCommands(argv[1]);
 }
 
+// * Function definitions
+
+// Function that gets the number of rows in a file
 int numRows(int file, int* errorDetected){
+    // Seek to the beginning of the file
     if(lseek(file, 0, SEEK_SET) == -1){
         errorDetected = -1;
         return;
@@ -34,16 +47,21 @@ int numRows(int file, int* errorDetected){
 
     int lines = 0;
     char symbol;
+
+    // Read the first symbol
     int readStatus = read(file, &symbol, 1);
     if(readStatus == -1){
         errorDetected = -2;
         return;
     }
 
+    // While there are symbols to read
     while(readStatus == 1){
+        // If the symbol is a new line, increment the number of rows
         if(symbol == '\n'){
             lines++;
         }
+        // Read the next symbol
         readStatus = read(file, &symbol, 1);
         if(readStatus == -1){
             errorDetected = -2;
@@ -51,14 +69,19 @@ int numRows(int file, int* errorDetected){
         }
     }
 
+    // Increment the number of rows to account for the last row
     lines++;
 
+    // Return the number of rows
     return lines;
 }
 
+// Function that gets the size of each row in a file
 int* sizeRows(int file, int* errorDetected){
+    // Get the number of rows in the file
     int numOfRows = numRows(file, errorDetected);
 
+    // Allocate memory for the array that will hold the size of each row
     int* sizeOfRows = (int*)malloc(numOfRows * sizeof(int));
     if(sizeOfRows == NULL){
         errorDetected = -3;
@@ -68,26 +91,34 @@ int* sizeRows(int file, int* errorDetected){
     int rowIndex = 0;
     int charIndex = 0;
 
+    // Seek to the beginning of the file
     if(lseek(file, 0, SEEK_SET) == -1){
         errorDetected = -1;
         return;
     }
 
     char symbol;
+
+    // Read the first symbol
     int readStatus = read(file, &symbol, 1);
     if(readStatus == -1){
         errorDetected = -2;
         return;
     }
 
+    // While there are symbols to read
     while(readStatus == 1){
+        // If the symbol is a new line, increment the row index and reset the character index
         if(symbol == '\n'){
             sizeOfRows[rowIndex] = charIndex;
             rowIndex++;
             charIndex = 0;
-        } else {
+        } 
+        // If the symbol is not a new line, increment the character index
+        else {
             charIndex++;
         }
+        // Read the next symbol
         readStatus = read(file, &symbol, 1);
         if(readStatus == -1){
             errorDetected = -2;
@@ -98,18 +129,25 @@ int* sizeRows(int file, int* errorDetected){
     return sizeOfRows;
 }
 
+// Function that gets the rows of a file
 char** getRows(int file, int* errorDetected){
+    // Get the number of rows in the file
     int numOfRows = numRows(file, errorDetected);
 
+    // Allocate memory for the array that will hold the rows
     char** rows = (char**)malloc((numOfRows + 1) * sizeof(char*));
     if(rows == NULL){
         errorDetected = -3;
         return;
     }
+
+    // Set the last element of the array to NULL
     rows[numOfRows] = NULL;
 
+    // Get the size of each row
     int* sizeOfRows = sizeRows(file, errorDetected);
 
+    // Allocate memory for each row
     for(int i = 0; i < numOfRows; i++){
         rows[i] = (char*)malloc((sizeOfRows[i] + 1) * sizeof(char));
         if(rows[i] == NULL){
@@ -122,26 +160,33 @@ char** getRows(int file, int* errorDetected){
     int rowIndex = 0;
     int charIndex = 0;
 
+    // Seek to the beginning of the file
     if(lseek(file, 0, SEEK_SET) == -1){
         errorDetected = -1;
         return;
     }
 
     char symbol;
+    // Read the first symbol
     int readStatus = read(file, &symbol, 1);
     if(readStatus == -1){
         errorDetected = -2;
         return;
     }
 
+    // While there are symbols to read
     while(readStatus == 1){
+        // If the symbol is a new line, increment the row index and reset the character index
         if(symbol == '\n'){
             rowIndex++;
             charIndex = 0;
-        } else {
+        }
+        // If the symbol is not a new line, add it to the current row and increment the character index 
+        else {
             rows[rowIndex][charIndex] = symbol;
             charIndex++;
         }
+        // Read the next symbol
         readStatus = read(file, &symbol, 1);
         if(readStatus == -1){
             errorDetected = -2;
@@ -152,23 +197,30 @@ char** getRows(int file, int* errorDetected){
     return rows;
 }
 
+// Function that gets the number of words in a row
 int numWords(char* row, int* errorDetected){
     int numOfWords = 0;
 
+    // Count the number of spaces in the row
     for(int i = 0; row[i] != '\0'; i++){
         if(row[i] == ' '){
             numOfWords++;
         }
     }
 
+    // Increment the number of words to account for the last word
     numOfWords++;
 
+    // Return the number of words
     return numOfWords;
 }
 
+// Function that gets the size of each word in a row
 int* sizeWords(char* row, int* errorDetected){
+    // Get the number of words in the row
     int numOfWords = numWords(row, errorDetected);
 
+    // Allocate memory for the array that will hold the size of each word
     int* sizeOfWords = (int*)malloc(numOfWords * sizeof(int));
     if(sizeOfWords == NULL){
         errorDetected = -3;
@@ -178,6 +230,7 @@ int* sizeWords(char* row, int* errorDetected){
     int wordSize = 0;
     int wordIndex = 0;
 
+    // Count the number of characters in each word
     for(int i = 0; row[i] != '\0'; i++){
         if(row[i] == ' '){
             sizeOfWords[wordIndex] = wordSize;
@@ -188,23 +241,31 @@ int* sizeWords(char* row, int* errorDetected){
         }
     }
 
+    // Add the size of the last word to the array
     sizeOfWords[wordIndex] = wordSize;
 
+    // Return the array
     return sizeOfWords;
 }
 
+// Function that gets the words of a row
 char** getWords(char* row, int* errorDetected){
+    // Get the number of words in the row
     int numOfWords = numWords(row, errorDetected);
 
+    // Allocate memory for the array that will hold the words
     char** words = (char**)malloc((numOfWords + 1) * sizeof(char*));
     if(words == NULL){
         errorDetected = -3;
         return;
     }
+    // Set the last element of the array to NULL
     words[numOfWords] = NULL;
 
+    // Get the size of each word
     int* sizeOfWords = sizeWords(row, errorDetected);
 
+    // Allocate memory for each word
     for(int i = 0; i < numOfWords; i++){
         words[i] = (char*)malloc((sizeOfWords[i] + 1) * sizeof(char));
         if(words[i] == NULL){
@@ -217,6 +278,7 @@ char** getWords(char* row, int* errorDetected){
     int wordIndex = 0;
     int charIndex = 0;
 
+    // Get each word
     for(int i = 0; row[i] != '\0'; i++){
         if(row[i] == ' '){
             wordIndex++;
@@ -227,9 +289,11 @@ char** getWords(char* row, int* errorDetected){
         }
     }
 
+    // Return the array
     return words;
 }
 
+// Error handler
 void errorHandler(int errorDetected){
     if(errorDetected == -1){
         err(1, "Error seeking in file");
@@ -248,19 +312,25 @@ void errorHandler(int errorDetected){
     }
 }
 
+// Function that executes the commands from a file
 void executeCommands(char* filename, int* errorDetected){
-    int fd = open(filename, O_RDONLY);
+    // Open the file
     int errorDetected = 0;
+    int fd = open(filename, O_RDONLY);
+    // If the file could not be opened, print an error message and exit
     if(fd == -1){
         err(1, "Error opening file");
     }
 
+    // Get the number of rows in the file
     int numOfRows = numRows(fd, errorDetected);
     if(errorDetected != 0){
         errorHandler(errorDetected);
     }
+    // Get the rows of the file
     char** rows = getRows(fd, errorDetected);
     if(errorDetected != 0){
+        // Free the memory allocated for the rows
         for(int i = 0; i < numOfRows; i++){
             free(rows[i]);
         }
@@ -270,14 +340,17 @@ void executeCommands(char* filename, int* errorDetected){
     }
 
     for(int i = 0; i < numOfRows; i++){
+        // Get the words of the current row
         char** words = getWords(rows[i], errorDetected);
 
         if(errorDetected != 0){
+            // Free the memory allocated for the words
             for(int j = 0; j < numWords(rows[i], errorDetected); j++){
                 free(words[j]);
             }
             free(words);
 
+            // Free the memory allocated for the rows
             for(int j = 0; j < i; j++){
                 free(rows[j]);
             }
@@ -286,16 +359,20 @@ void executeCommands(char* filename, int* errorDetected){
             errorHandler(errorDetected);
         }
         
+        // Fork the current process
         int childPid = fork();
 
+        // If the fork failed
         if(childPid == -1){
             errorDetected = -4;
 
+            // Free the memory allocated for the words
             for(int j = 0; j < numWords(rows[i], errorDetected); j++){
                 free(words[j]);
             }
             free(words);
 
+            // Free the memory allocated for the rows
             for(int j = 0; j < i; j++){
                 free(rows[j]);
             }
@@ -303,7 +380,9 @@ void executeCommands(char* filename, int* errorDetected){
 
             errorHandler(errorDetected);
         }
+        // If the current process is the child
         else if(childPid == 0){
+            // Execute the command and check for errors
             if(execvp(words[0], words) == -1){
                 errorDetected = -5;
 
@@ -320,7 +399,9 @@ void executeCommands(char* filename, int* errorDetected){
                 errorHandler(errorDetected);
             }
         }
+        // If the current process is the parent
         else{
+            // Wait for the child to finish
             int status;
             int child = wait(&status);
 
@@ -329,7 +410,9 @@ void executeCommands(char* filename, int* errorDetected){
             }
         }
 
+        // If the current row is the last one
         if(i == numOfRows - 1){
+            // Free the memory allocated for the words
             for(int j = 0; j < numWords(rows[i], errorDetected); j++){
                 free(words[j]);
             }
@@ -337,6 +420,7 @@ void executeCommands(char* filename, int* errorDetected){
         }
     }
 
+    // Free the memory allocated for the rows
     for(int i = 0; i < numOfRows; i++){
         free(rows[i]);
     }
