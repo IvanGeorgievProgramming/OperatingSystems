83bfdb22ee34ae5d83c390b83f0cd0468ee24777
Ivan Georgiev Programming <IvanGeorgievProgramming@gmail.com>
2023-01-21 20:18:26 +0200

    Ready?

--- CommandInterpreter/main.c
+++ CommandInterpreter/main.c
@@ -11,42 +11,20 @@
 #include<stdint.h>
 #include<time.h>
 #include<err.h>
+#include<sys/wait.h>
 
 int numRows(int file);
+int* sizeRows(int file);
+char** getRows(int file);
 int numWords(char* row);
-int* sizeOfWords(char* row);
+int* sizeWords(char* row);
 char** getWords(char* row);
-void executeCommands();
+void executeCommands(char* filename);
 
 int main(int argc, char *argv[]){
-    // Make a string
-    char* str = "Hello World , my name is Ivan";
-
-    // Get the number of words in the string
-    int numOfWords = numWords(str);
-    printf("The words are: %d\n", numOfWords);
-
-    // Get the size of each word in the string
-    int* sizeOfWordsArr = sizeOfWords(str);
-    printf("The size of each word is: ");
-    for (int i = 0; i < numOfWords; i++){
-        printf("%d ", sizeOfWordsArr[i]);
-    }
-    printf("\n");
-
-    // Get all the words in the string
-    char** words = getWords(str);
-    printf("The words are: ");
-    for (int i = 0; i < numOfWords; i++){
-        printf("%s ", words[i]);
-    }
-    printf("\n");
-
-    // Get the number of rows in a file
-    executeCommands();
+    executeCommands("CommandInterpreter/commands");
 }
 
-// Make a function that counts the number of rows in a file.
 int numRows(int file){
     lseek(file, 0, SEEK_SET);
 
@@ -68,7 +46,68 @@ int numRows(int file){
     return lines;
 }
 
-// Make a function that counts the number of words in a string.
+int* sizeRows(int file){
+    int numOfRows = numRows(file);
+
+    int* sizeOfRows = (int*)malloc(numOfRows * sizeof(int));
+
+    int rowIndex = 0;
+    int charIndex = 0;
+
+    lseek(file, 0, SEEK_SET);
+
+    char symbol;
+    int readStatus = read(file, &symbol, 1);
+
+    while(readStatus == 1){
+        if(symbol == '\n'){
+            sizeOfRows[rowIndex] = charIndex;
+            rowIndex++;
+            charIndex = 0;
+        } else {
+            charIndex++;
+        }
+        readStatus = read(file, &symbol, 1);
+    }
+
+    return sizeOfRows;
+}
+
+char** getRows(int file){
+    int numOfRows = numRows(file);
+
+    char** rows = (char**)malloc((numOfRows + 1) * sizeof(char*));
+    rows[numOfRows] = NULL;
+
+    int* sizeOfRows = sizeRows(file);
+
+    for(int i = 0; i < numOfRows; i++){
+        rows[i] = (char*)malloc((sizeOfRows[i] + 1) * sizeof(char));
+        rows[i][sizeOfRows[i]] = '\0';
+    }
+
+    int rowIndex = 0;
+    int charIndex = 0;
+
+    lseek(file, 0, SEEK_SET);
+
+    char symbol;
+    int readStatus = read(file, &symbol, 1);
+
+    while(readStatus == 1){
+        if(symbol == '\n'){
+            rowIndex++;
+            charIndex = 0;
+        } else {
+            rows[rowIndex][charIndex] = symbol;
+            charIndex++;
+        }
+        readStatus = read(file, &symbol, 1);
+    }
+
+    return rows;
+}
+
 int numWords(char* row){
     int numOfWords = 0;
 
@@ -83,8 +122,7 @@ int numWords(char* row){
     return numOfWords;
 }
 
-// Make a function that counts the size of each word in a string.
-int* sizeOfWords(char* row){
+int* sizeWords(char* row){
     int numOfWords = numWords(row);
 
     int* sizeOfWords = (int*)malloc(numOfWords * sizeof(int));
@@ -107,18 +145,17 @@ int* sizeOfWords(char* row){
     return sizeOfWords;
 }
 
-// Make a function that returns all the words in a string.
 char** getWords(char* row){
     int numOfWords = numWords(row);
 
     char** words = (char**)malloc((numOfWords + 1) * sizeof(char*));
     words[numOfWords] = NULL;
 
-    int* sizeOfWordsArr = sizeOfWords(row);
+    int* sizeOfWords = sizeWords(row);
 
     for(int i = 0; i < numOfWords; i++){
-        words[i] = (char*)malloc((sizeOfWordsArr[i] + 1) * sizeof(char));
-        words[i][sizeOfWordsArr[i]] = '\0';
+        words[i] = (char*)malloc((sizeOfWords[i] + 1) * sizeof(char));
+        words[i][sizeOfWords[i]] = '\0';
     }
 
     int wordIndex = 0;
@@ -137,13 +174,35 @@ char** getWords(char* row){
     return words;
 }
 
-// Make a function to execute the commands.
-void executeCommands(){
-    int fd = open("CommandInterpreter/commands", O_RDONLY);
+void executeCommands(char* filename){
+    int fd = open(filename, O_RDONLY);
     if(fd == -1){
         err(1, "Error opening file");
     }
 
     int numOfRows = numRows(fd);
-    printf("The number of rows is: %d\n", numOfRows);
+    char** rows = getRows(fd);
+
+    for(int i = 0; i < numOfRows; i++){
+        char** words = getWords(rows[i]);
+        
+        int childPid = fork();
+
+        if(childPid == -1){
+            err(1, "Error forking");
+        }
+        else if(childPid == 0){
+            if(execvp(words[0], words) == -1){
+                err(1, "Error executing command");
+            }
+        }
+        else{
+            int status;
+            int child = wait(&status);
+
+            if(WIFEXITED(status)){
+                printf("Child %d exited with status %d\n", child, WEXITSTATUS(status));
+            }
+        }
+    }
 }
